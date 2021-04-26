ff870e7e03b3d85e3b0f3172242cf7587e68ea73
zhangxin <zhangxin19870504@163.com>
2021-04-27 00:15:37 +0800

    git  config

---
+++ yc_dell/ubuntu.sh
@@ -0,0 +1,6 @@
+
+#export PATH="/usr/local/cuda-10.1/bin:$PATH"
+#export LD_LIBRARY_PATH="/usr/local/cuda-10.1/lib64:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+source /home/zhangxin/my-shell/yc_dell/cuda.sh
+source /home/zhangxin/my-shell/yc_dell/conda.sh
+

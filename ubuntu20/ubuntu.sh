5ae21a8be0288e4d7b115bef9433a34b4d394038
zhangxinnan <zhangxin19870504@163.com>
2021-11-07 11:27:13 +0800

    ubuntu20

---
+++ ubuntu20/ubuntu.sh
@@ -0,0 +1,6 @@
+
+# export PATH="/usr/local/cuda-10.1/bin:$PATH"
+# export LD_LIBRARY_PATH="/usr/local/cuda-10.1/lib64:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+source /home/zhangxin/my-shell/ubuntu20/cuda.sh
+source /home/zhangxin/my-shell/ubuntu20/conda.sh
+

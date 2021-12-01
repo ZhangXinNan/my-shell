12fd6a4c021fb83b70a33b9d685a0443390bb686
zhangxinnan <zhangxin19870504@163.com>
2021-12-02 05:05:35 +0800

    在曾一林的机器上安装tensorrt

---
+++ yc_dell/tensorrt.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/usr/local/TensorRT-7.2.3.4/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/TensorRT-7.2.3.4/lib
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/TensorRT-7.2.3.4/include
+

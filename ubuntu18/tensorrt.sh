7947d4542dc17a3e36a5617c056fbdd210794267
zhangxin <zhangxin19870504@163.com>
2021-12-01 11:00:37 +0800

    tensorrt

---
+++ ubuntu18/tensorrt.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/usr/local/TensorRT-7.2.2.3/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/TensorRT-7.2.2.3/lib
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/TensorRT-7.2.2.3/include
+
--- ubuntu18/ubuntu.sh
+++ ubuntu18/ubuntu.sh
@@ -3,4 +3,5 @@
 # export LD_LIBRARY_PATH="/usr/local/cuda-10.1/lib64:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
 source /home/zhangxin/my-shell/yc_dell/cuda.sh
 source /home/zhangxin/my-shell/yc_dell/conda.sh
+source /home/zhangxin/my-shell/ubuntu18/tensorrt.sh
 

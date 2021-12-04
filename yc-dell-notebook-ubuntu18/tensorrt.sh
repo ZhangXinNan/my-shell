2e9a5eb1eadc7cebf3f804e1ad86f039808ae81f
zhangxinnan <zhangxin19870504@163.com>
2021-12-04 12:58:05 +0800

    yc-dell-notebook-ubuntu18

---
+++ yc-dell-notebook-ubuntu18/tensorrt.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/usr/local/TensorRT-7/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/TensorRT-7/lib:/usr/local/TensorRT-7/targets/x86_64-linux-gnu/lib
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/TensorRT-7/include:/usr/local/TensorRT-7/targets/x86_64-linux-gnu/include
+

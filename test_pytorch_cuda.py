d7307e84c4064c3a8a1d7261d21c2c209ea6e19d
zhangxinnan <zhangxin19870504@163.com>
2022-06-25 22:11:54 +0800

    pytorch cuda version

--- test_pytorch_cuda.py
+++ test_pytorch_cuda.py
@@ -2,7 +2,13 @@
 
 import torch
 
-
+print(torch.version) # PyTorch version
 print(torch.cuda.is_available())
+
+print(torch.version.cuda) # Corresponding CUDA version
+
+print(torch.backends.cudnn.version()) # Corresponding cuDNN version
+
+print(torch.cuda.get_device_name(0)) # GPU type
 
 

2ace39446c456975a33d049fef235e63516b834b
zhangxinnan <zhangxin19870504@163.com>
2023-05-05 15:17:13 +0800

    ubuntu22 alienware

---
+++ ubuntu22/cuda.sh
@@ -0,0 +1,10 @@
+
+#Please make sure that
+# -   PATH includes /usr/local/cuda-11.2/bin
+# -   LD_LIBRARY_PATH includes /usr/local/cuda-11.2/lib64, or, add /usr/local/cuda-11.2/lib64 to /etc/ld.so.conf and run ldconfig as root
+
+
+export PATH=$PATH:/usr/local/cuda-11.7/bin
+export LD_LIBRARY_PATH=/usr/local/cuda-11.7/lib64
+
+
---
+++ ubuntu22/ubuntu.sh
@@ -0,0 +1,3 @@
+
+source /home/zhangxin/my-shell/ubuntu22/cuda.sh
+source /home/zhangxin/my-shell/ubuntu22/conda.sh
